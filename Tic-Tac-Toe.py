f27d9d3f275cba3b3a65ca77af160e7298e78d77
srivalli-148 <yssrivalliathreyi@gmail.com>
2026-04-08 22:33:39 +0530

    Add files via upload

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,20 +7,56 @@ running = True
 pygame.display.set_caption("Tic-Tac-Toe")
 icon = pygame.image.load('tic-tac-toe.png')
 pygame.display.set_icon(icon)
-BOARD = pygame.image.load('bruh.png')
 X_IMG = pygame.image.load('X.png')
 O_IMG = pygame.image.load('O.png')
-board = [[1,2,3],[4,5,6],[7,8,9]]
-graphical_board = [[[None,None],[None,None],[None,None]],
-                   [[None,None],[None,None],[None,None]],
-                   [[None,None],[None,None],[None,None]]]
+ROWS = 10
+COLS = 10
+CELL_SIZE = 60
+BOARD_X = 150
+BOARD_Y = 00
+def draw_board():
+    
+    for i in range(ROWS+1):
+        pygame.draw.line(screen,(0,0,0),(BOARD_X,BOARD_Y + i*CELL_SIZE),(BOARD_X+COLS*CELL_SIZE,BOARD_Y+i*CELL_SIZE),2)
+    
+    for j in range(COLS+1):
+        pygame.draw.line(screen,(0,0,0),(BOARD_X+j*CELL_SIZE,BOARD_Y),(BOARD_X+j*CELL_SIZE,BOARD_Y+ROWS*CELL_SIZE),2)
+
+#creates a 10x10 grid (list of lists)
+board = [[None for _ in range(10)] for _ in range(10)]
+#shows whose turn it is and first turn is of 'X'
 to_move = 'X'
-screen.fill((255,255,255))
-image_rect = BOARD.get_rect(center=(400,300))
-screen.blit(BOARD,image_rect)
-pygame.display.update()
+def draw_moves():
+    for row in range(10):
+        for col in range(10):
+            if board[row][col]=='X':
+                x=BOARD_X+col*CELL_SIZE
+                y=BOARD_Y+row*CELL_SIZE
+                screen.blit(X_IMG,(x,y))
+            elif board[row][col]=='O':
+                x=BOARD_X+col*CELL_SIZE
+                y=BOARD_Y+row*CELL_SIZE
+                screen.blit(O_IMG,(x,y))
 
 while running:
+    screen.fill((255,255,255))
+    draw_board()
+    draw_moves()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+
+            col = (mouse_x - BOARD_X)//CELL_SIZE
+            row = (mouse_y - BOARD_Y)//CELL_SIZE
+            if 0<=row<10 and 0<=col<10:
+                if board[row][col] is None:
+                    board[row][col]=to_move
+                    if to_move=='X':
+                        to_move='O'
+                    else:
+                        to_move='X'
+    
+    
+    pygame.display.update()
